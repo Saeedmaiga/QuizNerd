bd91633f3d427768e505a43397a0965ff35bb7ec
evanboyle47 <161370886+evanboyle47@users.noreply.github.com>
2025-11-24 14:45:11 -0500

    White screen after multiplayer back page fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -800,6 +800,7 @@ const playSound = useCallback((soundName) => {
     <ProfilePage
       onBack={() => {
         setShowProfile(false);
+        setShowQuizSelector(true);
         playSound("click");
       }}
       themeClasses={themeClasses}
